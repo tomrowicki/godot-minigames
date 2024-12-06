9d147074830ad6315a6f496a4a17580b82c24185
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-12-06 12:57:26 +0100

    Hitting the floor

--- tappy/scenes/plane/plane.gd
+++ tappy/scenes/plane/plane.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-const GRAVITY: float = 300.0
-const POWER: float = -200.0
+const GRAVITY: float = 1000.0
+const POWER: float = -350.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _ready() -> void:
 	pass
@@ -14,7 +17,14 @@ func _physics_process(delta: float) -> void:
 	# After setting the velocity we tell the physics engine to carry out the change;
 	# it multiplies velocity by delta, so ultimately it's delta squared(?)
 	move_and_slide()
+	
+	if is_on_floor():
+		die()
 
 func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = POWER
+
+func die() -> void:
+	animated_sprite.stop()
+	set_physics_process(false)
