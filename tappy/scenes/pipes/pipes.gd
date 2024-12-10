f7883a2b706c8c047a3ea6cc7a51dd9617f9f48d
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-12-10 12:05:56 +0100

    Scrolling pipes

---
+++ tappy/scenes/pipes/pipes.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Moving pipes left
+	position.x -= delta * 120.0
+	check_off_screen()
+
+func check_off_screen() -> void:
+	if position.x < -500.0:
+		queue_free()
+
+# Does not fire when the scene is not being rendered (e. g. the window is minimised)
+func _on_screen_exited() -> void:
+	queue_free()
