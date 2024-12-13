e58803bada6705c666534d5c23eb6f1d4074e99d
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-12-13 15:40:25 +0100

    Autoload (singleton) introduced

--- tappy/scenes/pipes/pipes.gd
+++ tappy/scenes/pipes/pipes.gd
@@ -2,14 +2,16 @@ extends Node2D
 
 class_name Pipes
 
+const OFF_SCREEN: float = -500.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Moving pipes left
-	position.x -= delta * 120.0
+	position.x -= delta * GameManager.SCROLL_SPEED
 	check_off_screen()
 
 func check_off_screen() -> void:
-	if position.x < -500.0:
+	if position.x < OFF_SCREEN:
 		queue_free()
 
 # Does not fire when the scene is not being rendered (e. g. the window is minimised)
---
+++ tappy/singletons/game_manager.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# It is made to autoload via project settings -> Globals
+
+const SCROLL_SPEED: float = 120.0
