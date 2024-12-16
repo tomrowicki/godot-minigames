ff47d2501a4f712fc6ee3dc4e9103c9773377d29
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-12-16 13:10:13 +0100

    Pipes and laser collisions

--- tappy/scenes/pipes/pipes.gd
+++ tappy/scenes/pipes/pipes.gd
@@ -3,6 +3,8 @@ extends Node2D
 class_name Pipes
 
 const OFF_SCREEN: float = -500.0
+@onready var score_sound: AudioStreamPlayer2D = $ScoreSound
+
 
 func _ready() -> void:
 	SignalManager.on_plane_died.connect(on_plane_died)
@@ -23,3 +25,14 @@ func _on_screen_exited() -> void:
 	
 func on_plane_died() -> void:
 	set_process(false)
+
+
+func _on_pipe_body_entered(body: Node2D) -> void:
+	# if body is Tappy:
+	if body.is_in_group(GameManager.GROUP_PLANE):
+		body.die()
+
+
+func _on_laser_body_entered(body: Node2D) -> void:
+	if body is Tappy:
+		score_sound.play()
--- tappy/singletons/game_manager.gd
+++ tappy/singletons/game_manager.gd
@@ -3,3 +3,4 @@ extends Node
 # It is made to autoload via project settings -> Globals
 
 const SCROLL_SPEED: float = 120.0
+const GROUP_PLANE: String = "Plane"
