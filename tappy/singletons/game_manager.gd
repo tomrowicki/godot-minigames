2f43306d2cf2b81f5ad33120ee28f4a04ee06179
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-12-19 12:44:10 +0100

    Main scene switching

--- tappy/singletons/game_manager.gd
+++ tappy/singletons/game_manager.gd
@@ -2,5 +2,10 @@ extends Node
 
 # It is made to autoload via project settings -> Globals
 
+const GAME: PackedScene = preload("res://scenes/game/game.tscn")
+
 const SCROLL_SPEED: float = 120.0
 const GROUP_PLANE: String = "Plane"
+
+func load_game_scene() -> void:
+	get_tree().change_scene_to_packed(GAME)
