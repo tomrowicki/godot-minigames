e091d834a70096d511e1128916f7aa3b58643fbf
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-12-20 14:35:06 +0100

    Game over UI and logic

--- tappy/singletons/game_manager.gd
+++ tappy/singletons/game_manager.gd
@@ -3,9 +3,13 @@ extends Node
 # It is made to autoload via project settings -> Globals
 
 const GAME: PackedScene = preload("res://scenes/game/game.tscn")
+const MAIN: PackedScene = preload("res://scenes/main/main.tscn")
 
 const SCROLL_SPEED: float = 120.0
 const GROUP_PLANE: String = "Plane"
 
 func load_game_scene() -> void:
 	get_tree().change_scene_to_packed(GAME)
+	
+func load_main_scene() -> void:
+	get_tree().change_scene_to_packed(MAIN)
