ece8ede80add6709c90155352eb2dff5caa17b75
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-01-04 15:53:53 +0100

    Animal state

--- angry-animals/scenes/animal/animal.gd
+++ angry-animals/scenes/animal/animal.gd
@@ -1,5 +1,10 @@
 extends RigidBody2D
 
+enum ANIMAL_STATE { READY, DRAG, RELEASE }
+
+@onready var label: Label = $Label
+
+var _state: ANIMAL_STATE = ANIMAL_STATE.READY
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,7 +12,35 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	pass
+	update(delta)
+	label.text = "%s" % ANIMAL_STATE.keys()[_state]
+	
+func set_new_state(new_state: ANIMAL_STATE) -> void:
+	_state = new_state
+	if _state == ANIMAL_STATE.RELEASE:
+		freeze = false
+	elif _state == ANIMAL_STATE.DRAG:
+		pass
+	
+func detect_release() -> bool:
+	if _state == ANIMAL_STATE.DRAG:
+		if Input.is_action_just_released("drag"):
+			set_new_state(ANIMAL_STATE.RELEASE)
+			return true
+			
+	return false
+	
+func update_drag() -> void:
+	if detect_release():
+		return
+	
+	var gmp = get_global_mouse_position()
+	position = gmp
+	
+func update(delta: float) -> void:
+	match _state:
+		ANIMAL_STATE.DRAG:
+			update_drag()
 
 func die() -> void:
 	SignalManager.on_animal_died.emit()
@@ -15,3 +48,8 @@ func die() -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	die()
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if _state == ANIMAL_STATE.READY and event.is_action_pressed("drag"):
+		set_new_state(ANIMAL_STATE.DRAG)
