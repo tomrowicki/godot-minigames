1f2565184ef423e0ca88a1bf07ddf198e7415977
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-01-10 14:37:59 +0100

    Level selection

--- angry-animals/scenes/level/level.gd
+++ angry-animals/scenes/level/level.gd
@@ -1,18 +1,20 @@
 extends Node2D
 
 const ANIMAL: PackedScene = preload("res://scenes/animal/animal.tscn")
-@onready var animal_start: Marker2D = $AnimalStart
+const MAIN = preload("res://scenes/main/main.tscn")
 
+
+@onready var animal_start: Marker2D = $AnimalStart
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_animal()
 	SignalManager.on_animal_died.connect(add_animal)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().change_scene_to_packed(MAIN)
 	
 func add_animal() -> void:
 	var animal: Node = ANIMAL.instantiate()
---
+++ angry-animals/scenes/level_button/level_button.gd
@@ -0,0 +1,27 @@
+extends TextureButton
+
+const HOVER_SCALE: Vector2 = Vector2(1.1, 1.1)
+const DEFAULT_SCALE: Vector2 = Vector2(1.0, 1.0)
+
+@export var level_number: int = 1
+
+@onready var level_label: Label = $MarginContainer/VBoxContainer/LevelLabel
+@onready var score_label: Label = $MarginContainer/VBoxContainer/ScoreLabel
+
+var _level_scene: PackedScene
+
+# Called three times (once for each of the buttons)
+func _ready() -> void:
+	_level_scene = load("res://scenes/level/level%s.tscn" % level_number) 
+	level_label.text = str(level_number)
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_packed(_level_scene)
+
+
+func _on_mouse_entered() -> void:
+	scale = HOVER_SCALE
+
+
+func _on_mouse_exited() -> void:
+	scale = DEFAULT_SCALE
