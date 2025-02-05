5b9ed6a08cfa9d4d1e5ce61f5ea14a398389b949
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-02-05 13:11:54 +0100

    Basic player movement

--- foxy-antics/scenes/player/player.gd
+++ foxy-antics/scenes/player/player.gd
@@ -5,6 +5,11 @@ class_name Player
 
 
 const GRAVITY: float = 690.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 400.0
+const JUMP_VELOCITY: float = -260.0
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,4 +22,17 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor() == false:
 		velocity.y += GRAVITY * delta
 		
+	get_input()
 	move_and_slide()
+
+
+func get_input() -> void:
+	velocity.x = 0
+	
+	if Input.is_action_pressed("left"):
+		velocity.x = -RUN_SPEED
+		sprite_2d.flip_h = true
+	
+	elif Input.is_action_pressed("right"):
+		velocity.x = RUN_SPEED
+		sprite_2d.flip_h = false
