94f46f20b0eeb7ff153d8a33b2228a01a6ed07ee
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-02-06 11:25:43 +0100

    Player jump and debug label

--- foxy-antics/scenes/player/player.gd
+++ foxy-antics/scenes/player/player.gd
@@ -10,6 +10,7 @@ const MAX_FALL: float = 400.0
 const JUMP_VELOCITY: float = -260.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var debug_label: Label = $DebugLabel
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,6 +25,11 @@ func _physics_process(delta: float) -> void:
 		
 	get_input()
 	move_and_slide()
+	update_debug_label()
+
+
+func update_debug_label() -> void:
+	debug_label.text = "floor:%s\n%.0f,%.0f" % [is_on_floor(), velocity.x, velocity.y]
 
 
 func get_input() -> void:
@@ -36,3 +42,8 @@ func get_input() -> void:
 	elif Input.is_action_pressed("right"):
 		velocity.x = RUN_SPEED
 		sprite_2d.flip_h = false
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
