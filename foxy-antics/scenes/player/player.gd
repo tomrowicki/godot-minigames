8ea901794daa98cc06dd9d2f911e2b971b8d6c2f
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-02-07 13:30:37 +0100

    Player states

--- foxy-antics/scenes/player/player.gd
+++ foxy-antics/scenes/player/player.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 class_name Player
 
 
+enum PlayerState {IDLE, RUN, JUMP, FALL, HURT}
+
+
+
 const GRAVITY: float = 690.0
 const RUN_SPEED: float = 120.0
 const MAX_FALL: float = 400.0
@@ -11,6 +15,10 @@ const JUMP_VELOCITY: float = -260.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var debug_label: Label = $DebugLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+var _state: PlayerState = PlayerState.IDLE
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,11 +33,13 @@ func _physics_process(delta: float) -> void:
 		
 	get_input()
 	move_and_slide()
+	calculate_states()
 	update_debug_label()
 
 
 func update_debug_label() -> void:
-	debug_label.text = "floor:%s\n%.0f,%.0f" % [is_on_floor(), velocity.x, velocity.y]
+	debug_label.text = "floor:%s\n%s\nvel:(%.0f,%.0f)" % [is_on_floor(), PlayerState.keys()[_state],
+	 velocity.x, velocity.y]
 
 
 func get_input() -> void:
@@ -47,3 +57,33 @@ func get_input() -> void:
 		velocity.y = JUMP_VELOCITY
 		
 	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
+
+
+func calculate_states() -> void:
+	if is_on_floor():
+		if velocity.x == 0:
+			set_state(PlayerState.IDLE)
+		else:
+			set_state(PlayerState.RUN)
+	else:
+		if velocity.y > 0:
+			set_state(PlayerState.FALL)
+		else:
+			set_state(PlayerState.JUMP)
+
+
+func set_state(new_state: PlayerState) -> void:
+	if new_state == _state:
+		return
+	
+	_state = new_state	
+
+	match _state:
+		PlayerState.IDLE:
+			animation_player.play("idle")
+		PlayerState.RUN:
+			animation_player.play("run")
+		PlayerState.JUMP:
+			animation_player.play("jump")
+		PlayerState.FALL:
+			animation_player.play("fall")
