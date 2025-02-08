eddc59a02f65cbb37369d7053b1e95d15cd87df6
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-02-08 12:54:05 +0100

    Bullet scenes

---
+++ foxy-antics/constants/constants.gd
@@ -0,0 +1,11 @@
+class_name Constants
+
+enum ObjectType {
+	EXPLOSION,
+	PICKUP,
+	BULLET_PLAYER,
+	BULLET_ENEMY
+}
+
+const PLAYER_GROUP: String = "Player"
+const MOVEABLES_GROUP: String = "Moveables"
