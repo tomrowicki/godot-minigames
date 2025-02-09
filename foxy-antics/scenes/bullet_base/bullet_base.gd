5392888288bfa5ee250a189d2a07b6f8aaadf5ab
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-02-09 17:13:08 +0100

    Bullet logic

---
+++ foxy-antics/scenes/bullet_base/bullet_base.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+class_name Bullet
+
+
+var _direction: Vector2 = Vector2(100, -50)
+var _life_span: float = 5.0
+var _life_time: float = 0.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += _direction * delta
+	check_expired(delta)
+
+
+func check_expired(delta: float) -> void:
+	_life_time += delta
+	if (_life_time > _life_span):
+		queue_free()
+		
+		
+func setup(pos: Vector2, dir: Vector2, speed: float, ls: float) -> void:
+	_direction = dir * speed
+	_life_span = ls
+	global_position = pos
+	
+
+func _on_area_entered(area: Area2D) -> void:
+	queue_free()
