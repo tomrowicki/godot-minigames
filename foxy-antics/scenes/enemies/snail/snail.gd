44e3a18f63ea650026ee69ea4d0dc2ccd813663e
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-02-19 17:44:10 +0100

    Snail logic and frog visuals

--- foxy-antics/scenes/enemies/snail/snail.gd
+++ foxy-antics/scenes/enemies/snail/snail.gd
@@ -1,11 +1,30 @@
 extends EnemyBase
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var floor_detection: RayCast2D = $FloorDetection
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+@export var speed: float = 50.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	
+	if !is_on_floor():
+		velocity.y += _gravity * delta
+	else:
+#		# use of ternary operation
+		velocity.x = speed if animated_sprite_2d.flip_h else -speed
+		
+	move_and_slide()
+	
+	if is_on_floor():
+		if is_on_wall() or !floor_detection.is_colliding():
+			flip_me()
+	
+	
+func flip_me() -> void:
+	# flipping the snal
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+	# flipping the raycast
+	floor_detection.position.x = -floor_detection.position.x
+	
