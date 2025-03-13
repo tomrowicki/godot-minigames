6b95ce83244c2cb07812847eb7e83e1926495373
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-03-13 15:10:00 +0100

    Checkpoint logic

--- foxy-antics/scenes/checkpoint/checkpoint.gd
+++ foxy-antics/scenes/checkpoint/checkpoint.gd
@@ -1,11 +1,27 @@
 extends Area2D
 
 
+const TRIGGER_CONDITION: String = "parameters/conditions/on_trigger"
+
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var sound: AudioStreamPlayer2D = $Sound
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	SignalManager.on_boss_killed.connect(on_boss_killed)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func on_boss_killed(points: int) -> void:
+	sprite_2d.show()
+	animation_tree[TRIGGER_CONDITION] = true
+	monitoring = true # start monitoring collisions
+	SoundManager.play_clip(sound, SoundManager.SOUND_CHECKPOINT)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Player completes level")
+	SoundManager.play_clip(sound, SoundManager.SOUND_WIN)
