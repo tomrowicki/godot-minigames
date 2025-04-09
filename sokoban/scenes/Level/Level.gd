a1077f4794778c700f62f54b3de7f7a218193a49
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-04-09 17:30:16 +0200

    Player camera and movement

--- sokoban/scenes/Level/Level.gd
+++ sokoban/scenes/Level/Level.gd
@@ -9,8 +9,30 @@ const SOURCE_ID: int = 0
 @onready var walls_tiles: TileMapLayer = $TileLayers/Walls
 @onready var targets_tiles: TileMapLayer = $TileLayers/Targets
 @onready var boxes_tiles: TileMapLayer = $TileLayers/Boxes
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var player: AnimatedSprite2D = $Player
+
 
 var _tile_size: int = 0
+var _player_tile: Vector2i = Vector2i.ZERO
+
+
+func get_input_direction() -> Vector2i:
+	# move direction
+	var md: Vector2i = Vector2i.ZERO
+	
+	if Input.is_action_just_pressed("ui_left"):
+		md = Vector2i.LEFT
+		player.flip_h = true
+	elif Input.is_action_just_pressed("ui_right"):
+		md = Vector2i.RIGHT
+		player.flip_h = false
+	elif Input.is_action_just_pressed("ui_up"):
+		md = Vector2i.UP
+	elif Input.is_action_just_pressed("ui_down"):
+		md = Vector2i.DOWN
+	
+	return md
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -19,11 +41,24 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("reload"):
 		get_tree().reload_current_scene()
 		
+	var md: Vector2i = get_input_direction()
+	if md != Vector2i.ZERO:
+		player_move(md)
+		
+		
+func player_move(md: Vector2i) -> void:
+	place_player_on_tile(_player_tile + md)
+	
 
 func _ready() -> void:
 	print("Level Loaded: ", GameManager.get_level_selected())
 	_tile_size = floor_tiles.tile_set.tile_size.x
 	setup_level()
+	
+	
+func place_player_on_tile(tile_coord: Vector2i) -> void:
+	player.position = Vector2(tile_coord * _tile_size)
+	_player_tile = tile_coord
 	
 	
 func get_atlas_coord(lt: TileLayers.LayerType) -> Vector2i:
@@ -57,6 +92,12 @@ func clear_tiles() -> void:
 	for tl in tile_layers.get_children():
 		tl.clear()
 	
+
+func move_camera() -> void:
+	# tilemap rect
+	var tmr: Rect2i = floor_tiles.get_used_rect()
+	camera_2d.position = tmr.get_center() * _tile_size
+	
 	
 func setup_level() -> void:
 	var level_number: String = GameManager.get_level_selected()
@@ -68,3 +109,7 @@ func setup_level() -> void:
 	setup_layer(TileLayers.LayerType.Targets, targets_tiles, level_layout)
 	setup_layer(TileLayers.LayerType.Boxes, boxes_tiles, level_layout)
 	setup_layer(TileLayers.LayerType.TargetBoxes, boxes_tiles, level_layout)
+	
+	place_player_on_tile(level_layout.get_player_start())
+	
+	move_camera()
