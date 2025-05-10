e0cc8c7942e2ab7c5e8abe386538a53bbcfb52f1
Tomasz Mrowicki <tomrowicki@gmail.com>
2025-05-10 14:42:39 +0200

    Patrolling

--- do-not-mind-me/scenes/Npc/Npc.gd
+++ do-not-mind-me/scenes/Npc/Npc.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
 
+enum EnemyState {Patrolling, Chasing, Searching}
+
+
 const SPEED: float = 100.0
+
+
+@export var patrol_points: NodePath
 
 
 @onready var nav_agent: NavigationAgent2D = $NavAgent
 @onready var debug_label: Label = $DebugLabel
+
+
+var _waypoints: Array[Vector2] = []
+var _current_wp: int = 0
+var _state: EnemyState = EnemyState.Patrolling
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -14,16 +25,30 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _ready() -> void:
+	create_wp()
 	pass
+	
+func create_wp() -> void:
+	for c in get_node(patrol_points).get_children():
+		_waypoints.append(c.global_position)
 	
 
 func _physics_process(delta: float) -> void:
+	update_movement()
 	update_navigation()
 	set_label()
+	
+	
+func navigate_wp() -> void:
+	if _waypoints.is_empty(): return
+	
+	nav_agent.target_position = _waypoints[_current_wp]
+	_current_wp = (_current_wp + 1) % _waypoints.size()
 
 
 func update_navigation() -> void:
-	if nav_agent.is_navigation_finished(): return
+	if nav_agent.is_navigation_finished():
+		return
 	
 	# next position in path
 	var npp: Vector2 = nav_agent.get_next_path_position()
@@ -35,11 +60,23 @@ func update_navigation() -> void:
 	move_and_slide()
 
 
+func process_patrolling() -> void:
+	if nav_agent.is_navigation_finished():
+		navigate_wp()
+
+
+func update_movement() -> void:
+	match _state:
+		EnemyState.Patrolling:
+			process_patrolling()
+
+
 func set_label() -> void:
 	var s: String = "Fin:%s\n" % nav_agent.is_navigation_finished()
 	s += "TG_REA:%s\n" % nav_agent.is_target_reached()
 	s += "CAN_REA:%s\n" % nav_agent.is_target_reachable()
-	s += "TAR:%s" % nav_agent.target_position
+	s += "TAR:%s\n" % nav_agent.target_position
+	s += "ST:%s" % EnemyState.keys()[_state]
 	
 	debug_label.text = s
 
